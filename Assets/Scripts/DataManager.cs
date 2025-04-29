9db7e7e8a30a30a34384fb6c5a33b8af475aa10f
TKG42 <153872863+TKG42@users.noreply.github.com>
2025-04-29 16:45:04 -0700

    Singleton added
    Singleton instance added to DataManager.cs. serializable player name and high scores, plus variable declarations added. Save path and empty saveData and LoadData() methods added.

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class SaveData
+{
+    public string playerName;
+    public int highScore;
+}
 
 public class DataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static DataManager Instance;
+    public string playerName;
+    public int highScore;
+    public string highScoreName;
+
+    private string savePath;
+
+    void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        savePath = Application.persistentDataPath + "/savefile.json";
+        LoadData();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveData()
     {
-        
+
+    }
+
+    public void LoadData()
+    {
+
     }
 }
